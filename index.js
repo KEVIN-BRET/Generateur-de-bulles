d16b57d7f12188245539bdca98d9d52021950cbf
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-04-01 18:52:20 +0200

    génération automatique des bulles

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const bubble = document.createElement("span"); // on initialise une const bubble qui va créer une <span> dans notre le <body> de notre html
-bubble.classList.add("bubble");
-document.body.appendChild(bubble); // on attribu la balise <span> (const bubble) comme élément de notre body
+const bubbleMaker = () => {
+  const bubble = document.createElement("span"); // on initialise une const bubble qui va créer une <span> dans notre le <body> de notre html
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble); // on attribu la balise <span> (const bubble) comme élément de notre body
 
-// console.log(Math.random()); // evoi un chiffe aléatoire entre 0 et 1
-// console.log(Math.random() * 200 + 100); // evoi un chiffe aléatoire entre 100 et 300
+  // console.log(Math.random()); // evoi un chiffe aléatoire entre 0 et 1
+  // console.log(Math.random() * 200 + 100); // evoi un chiffe aléatoire entre 100 et 300
 
-const size = Math.random() * 200 + 100 + "px";
-bubble.style.height = size;
-bubble.style.width = size;
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
 
-bubble.style.top = Math.random() * 100 + 50 + "%";
-bubble.style.left = Math.random() * 100 + "%";
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
 
-// bubble.style.setProperty("--left", Math.random() * 100 + "%"); // pour modifier la var CSS aleatoirement
+  // bubble.style.setProperty("--left", Math.random() * 100 + "%");
+  // pour modifier la var CSS aleatoirement
+  // mais les bulles partent trop a droite, donc :
 
-console.log(bubble);
+  const plusMinus = Math.random() > 0.5 ? 1 : -1; // pour renvoyer un (--left) négatif aléatoirement
+  bubble.style.setProperty("--left", Math.random() * 100 + plusMinus + "%"); // pour modifier la var CSS aleatoirement
+
+  //   setTimeout pour supprimer les bulles après 8s ..
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 2000); // appeler bubbleMaker toutes les 0.3s ...
