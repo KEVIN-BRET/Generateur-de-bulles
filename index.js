16dc332800cfbb39c173d2bf63fcdc70d9dcdbae
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-04-01 19:27:12 +0200

    validation html css + suppression des commentaires

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ let counter = 0;
 const bubbleMaker = () => {
   const bubble = document.createElement("span"); // on initialise une const bubble qui va créer une <span> dans notre le <body> de notre html
   bubble.classList.add("bubble");
-  document.body.appendChild(bubble); // on attribu la balise <span> (const bubble) comme élément de notre body
-
-  // console.log(Math.random()); // evoi un chiffe aléatoire entre 0 et 1
-  // console.log(Math.random() * 200 + 100); // evoi un chiffe aléatoire entre 100 et 300
+  document.body.appendChild(bubble);
 
   const size = Math.random() * 200 + 100 + "px";
   bubble.style.height = size;
@@ -16,26 +13,18 @@ const bubbleMaker = () => {
   bubble.style.top = Math.random() * 100 + 50 + "%";
   bubble.style.left = Math.random() * 100 + "%";
 
-  // bubble.style.setProperty("--left", Math.random() * 100 + "%");
-  // pour modifier la var CSS aleatoirement
-  // mais les bulles partent trop a droite, donc :
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 + plusMinus + "%");
 
-  const plusMinus = Math.random() > 0.5 ? 1 : -1; // pour renvoyer un (--left) négatif aléatoirement
-  bubble.style.setProperty("--left", Math.random() * 100 + plusMinus + "%"); // pour modifier la var CSS aleatoirement
-
-  // disparition des bulles au click
   bubble.addEventListener("click", () => {
     counter++;
-    console.log(counter);
     bubble.remove();
-    // counterDisplay.innerHTML = counter; // ou :
     counterDisplay.textContent = counter;
   });
 
-  //   setTimeout pour supprimer les bulles après 8s ..
   setTimeout(() => {
     bubble.remove();
   }, 8000);
 };
 
-setInterval(bubbleMaker, 1000); // appeler bubbleMaker toutes les 0.3s ...
+setInterval(bubbleMaker, 1000);
